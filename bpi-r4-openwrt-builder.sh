ada52d574b4ef3f752afc9e23ac7deedaffecc80
Petr Wozniak <156495286+woziwrt@users.noreply.github.com>
2025-04-12 22:13:48 +0200

    some comments

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#*****************************************************************************
+#
+# Build environment - Ubuntu 64-bit Server 24.04.2
+#
+# sudo apt update
+# sudo apt install build-essential clang flex bison g++ gawk \
+# gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev \
+# python3-setuptools rsync swig unzip zlib1g-dev file wget
+#
+#*****************************************************************************
+
+
 rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
