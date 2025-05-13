2e6b82fc778a8f6f7bbb27bf922ca343f3cea17c
Petr Wozniak <156495286+woziwrt@users.noreply.github.com>
2025-05-13 08:52:13 +0200

    mtk test

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -15,20 +15,21 @@ rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
 git clone --branch openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git openwrt || true
-cd openwrt; git checkout 3a481ae21bdc504f7f0325151ee0cb4f25dfd2cd; cd -;		#toolchain: mold: add PKG_NAME to Makefile
+#cd openwrt; git checkout 3a481ae21bdc504f7f0325151ee0cb4f25dfd2cd; cd -;		#toolchain: mold: add PKG_NAME to Makefile
+cd openwrt; git checkout 4a55d894fdddd028f36b552ee95ab30bc7a79bd4; cd -;		#kernel: modules: add HiNIC support
 
 git clone  https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
-cd mtk-openwrt-feeds; git checkout dfc7b1b3b65701e50db7fea9649e5b3373ec0795; cd -;	#Change to using a built-in module for the EN8811H 2.5G PHY driver
-#cd mtk-openwrt-feeds; git checkout 117d8018b98c348644258a3d8d1b1a2b933df28a; cd -;	#linux-firmware: Change firmware package folder
+#cd mtk-openwrt-feeds; git checkout dfbc4cbf5177b9291807d1d05d3edb76fe509755; cd -;	#Fix patch conflict issue
+cd openwrt; git checkout 930e04abfe39c3d80c255837ed66c01917f1f284; cd -;		#Add firmware encryption config
 
 #feeds modification
 #\cp -r my_files/w-feeds.conf.default openwrt/feeds.conf.default
 
 ### wireless-regdb modification - this remove all regdb wireless countries restrictions
-rm -rf openwrt/package/firmware/wireless-regdb/patches/*.*
-rm -rf mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches/*.*
-\cp -r my_files/500-tx_power.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches
-\cp -r my_files/regdb.Makefile openwrt/package/firmware/wireless-regdb/Makefile
+#rm -rf openwrt/package/firmware/wireless-regdb/patches/*.*
+#rm -rf mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches/*.*
+#\cp -r my_files/500-tx_power.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches
+#\cp -r my_files/regdb.Makefile openwrt/package/firmware/wireless-regdb/Makefile
 
 ### jumbo frames support
 #\cp -r my_files/750-mtk-eth-add-jumbo-frame-support-mt7998.patch openwrt/target/linux/mediatek/patches-6.6
