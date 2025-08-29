7e8d0662ba24051f46f04f80510d11099c8fc309
Petr Wozniak <156495286+woziwrt@users.noreply.github.com>
2025-08-29 23:19:34 +0200

    kernel 6.6.102 + crypto

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -2,7 +2,7 @@
 
 #*****************************************************************************
 #
-# Build environment - Ubuntu 64-bit Server 24.04.2
+# Build environment - Ubuntu 64-bit Desktop 25.04
 #
 # sudo apt update
 # sudo apt install build-essential clang flex bison g++ gawk \
@@ -16,13 +16,41 @@ rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
 git clone --branch openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git openwrt || true
-cd openwrt; git checkout d12f30f76006c2fc24164853acf0bc2ecf47a566; cd -;		#airoha: backport upstream patch for Flow Offload support for AN7581
+cd openwrt; git checkout a65ca44cb7a3e6fbb43b230c7c5a0684d88bae8b; cd -;		#kernel: bump 6.6 to 6.6.102
 
 git clone  https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
-cd mtk-openwrt-feeds; git checkout 1705eb8f024fde39047d1be1c9fd9c335162adff; cd -;	#Add mxl862 switch DSA tag 8021q offload support
+cd mtk-openwrt-feeds; git checkout b6114208501871ae977ab17c7d00ae8231266e99; cd -;	#Fix trng patch failed since upgrading to 6.6.102
 
-echo "1705eb" > mtk-openwrt-feeds/autobuild/unified/feed_revision
+echo "b611420" > mtk-openwrt-feeds/autobuild/unified/feed_revision
 
+#\cp -r configs/defconfig mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
+#\cp -r configs/dbg_defconfig mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig	#dbg+strongswan
+#\cp -r configs/dbg_defconfig_crypto mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
+
+#Change Feeds Revision
+#\cp -r my_files/w-feeds.conf.default openwrt/feeds.conf.default
+
+\cp -r my_files/w-rules mtk-openwrt-feeds/autobuild/unified/filogic/rules
+
+\cp -r my_files/0003-w-hostapd-package-makefile-ucode-files.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/patches-base/0003-hostapd-package-makefile-ucode-files.patch
+
+### remove mtk strongswan uci support patch
+rm -rf mtk-openwrt-feeds/24.10/patches-feeds/108-strongswan-add-uci-support.patch 
+
+### wireless-regdb modification - this remove all regdb wireless countries restrictions
+#rm -rf openwrt/package/firmware/wireless-regdb/patches/*.*
+#rm -rf mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches/*.*
+#\cp -r my_files/500-tx_power.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches
+#\cp -r my_files/regdb.Makefile openwrt/package/firmware/wireless-regdb/Makefile
+
+### adds a frequency match check to ensure the noise value corresponds to the interface's actual frequency for multiple radios under a single wiphy
+\cp -r my_files/200-wozi-libiwinfo-fix_noise_reading_for_radios.patch openwrt/package/network/utils/iwinfo/patches
+
+### tx_power patch - required for BE14 boards with defective eeprom flash
+\cp -r my_files/99999_tx_power_check.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/
+
+### required & thermal zone 
+\cp -r my_files/1007-wozi-arch-arm64-dts-mt7988a-add-thermal-zone.patch mtk-openwrt-feeds/24.10/patches-base/
 
 sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/defconfig
 sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-feeds/autobuild/autobuild_5.4_mac80211_release/mt7988_wifi7_mac80211_mlo/.config
@@ -32,4 +60,36 @@ cd openwrt
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 log_file=make
 
 exit 0
+
+
+########### After successful end of build #############
+
+## IMPORTANT NOTE !!!!!
+## Do not change Target Profile from Multiple devices to other  !!!
+## Do not remove MediaTek MT7988A rfb and MediaTek MT7988D rfb from Target Devices !!! 
+
+#################
+
+cd openwrt
+# Basic config
+\cp -r ../configs/mm.config .config
+
+
+###### Then you can add all required additional feeds/packages ######### 
+
+# qmi modems extension for example
+\cp -r ../my_files/luci-app-3ginfo-lite-main/sms-tool/ feeds/packages/utils/sms-tool
+\cp -r ../my_files/luci-app-3ginfo-lite-main/luci-app-3ginfo-lite/ feeds/luci/applications
+\cp -r ../my_files/luci-app-modemband-main/luci-app-modemband/ feeds/luci/applications
+\cp -r ../my_files/luci-app-modemband-main/modemband/ feeds/packages/net/modemband
+\cp -r ../my_files/luci-app-at-socat/ feeds/luci/applications
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+####### And finally configure whatever you want ##########
+
+make menuconfig
+make -j$(nproc)
+
 
