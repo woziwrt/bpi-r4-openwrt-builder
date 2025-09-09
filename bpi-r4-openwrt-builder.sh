f7c83441c499efb2b54ab4d906c907ef7ae70eaa
Petr Wozniak <156495286+woziwrt@users.noreply.github.com>
2025-09-09 11:34:20 +0200

    RC4_wc_test

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -16,13 +16,12 @@ rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
 git clone --branch openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git openwrt || true
-cd openwrt; git checkout b4b9288f2aa3dd1a759e5effbc8378f614bd5755; cd -;		#mac80211: Update to 6.12.44
+cd openwrt; git checkout bc9f1cab83d09f91e28c7ca830de778b08bd2842; cd -;		#libxml2: Add abi version
 
 git clone  https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
-cd mtk-openwrt-feeds; git checkout 787f5039cce935b94b11b6120c4014ad65e0dbb9; cd -;	#[openwrt-24][mt7988][fitblk][Fix fitblk create fitrw dev]
+cd mtk-openwrt-feeds; git checkout 939387af56de289422deb7196ab3be881aa807b9; cd -;	#Fix hostapd library recusive dependency
 
-
-echo "787f503" > mtk-openwrt-feeds/autobuild/unified/feed_revision
+echo "939387a" > mtk-openwrt-feeds/autobuild/unified/feed_revision
 
 
 ### wireless-regdb modification - this remove all regdb wireless countries restrictions
@@ -61,8 +60,8 @@ exit 0
 #################
 
 cd openwrt
-## RC3_crypto config
-\cp -r ../configs/mm_config .config
+## RC4 config
+\cp -r ../configs/RC4_mm_config_without_crypto .config
 
 
 ###### Then you can add all required additional feeds/packages ######### 
